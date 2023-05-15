e38ee917bea6b36dc8b43c7a3ee44644b37837b2
Zakariye Abdi <zak.abdi94@gmail.com>
2023-05-15 13:01:13 -0500

    made it look resemble a messenger app, created README file

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # typeScript-chat
-Chap application made with typescript
+
+# Chat Room
+
+Chat Room is a simple web application that allows users to communicate with each other in real-time. It provides a chat interface where users can send and receive messages.
+
+Throwback to jquery, reasons why i didnt use react were so I can have a little refresh of jQuery and basic HTML.
+
+## Features
+
+- Real-time messaging: Messages are sent and received instantly without the need to refresh the page.
+- Online user list: Displays a list of online users.
+- Message history: Messages are stored in the browser's local storage, so they are not lost on page refresh.
+
+## Technologies Used
+
+- HTML
+- CSS (Bootstrap)
+- JavaScript (jQuery)
+- Node.js
+- Express.js
+- Socket.IO
+
+## Getting Started
+
+### Prerequisites
+
+- Node.js (v12 or higher) and npm installed on your machine.
+
+### Installation
+
+1. Clone the repository:
+2. Navigate to the project directory:
+3. Install dependencies:
+4. Start the server:
+5. Open a web browser and visit `http://localhost:3000` to access the chat room.
--- server.js
+++ server.js
@@ -1,25 +1,31 @@
 const express = require("express");
-const { listen } = require("socket.io");
-var app = express();
-const server = require("http").createServer(app);
-const io = require("socket.io")(server);
-users = [];
-connections = [];
-server.listen(3000);
-app.get("/", function (req, res) {
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+var connections = [];
+
+app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
-io.sockets.on("connection", function (socket) {
-  // when clients connects to server
-  connections.push(socket); // add plug details to custom arrays
-  console.log("connected: %s socket connected", connections.length);
-  socket.on("disconnect", function (data) {
-    connections.splice(connections.indexOf(socket), 1); // delete plug details
-    console.log("disconnected : %s socket connected", connections.length);
+
+io.on("connection", (socket) => {
+  connections.push(socket);
+  console.log("a user connected");
+
+  socket.on("disconnect", function () {
+    connections.splice(connections.indexOf(socket), 1);
+    console.log("a user disconnected");
   });
+
   socket.on("send message", function (data) {
     console.log(data);
     io.sockets.emit("new message", { msg: data });
   });
+});
+
+server.listen(3000, () => {
+  console.log("listening on *:3000");
 });
 console.log("server is listening");
